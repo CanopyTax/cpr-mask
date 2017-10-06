4f91c4a3235ef656cda6ad798535d3639d1fd60c
Bret Little <bret.little@gmail.com>
2017-10-06 14:19:03 -0600

    Move onto prop-types package for React 16

--- src/cpr-mask.component.js
+++ src/cpr-mask.component.js
@@ -1,4 +1,5 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { find } from 'lodash';
 import { valueToMask, checkMasks, getPlaceholder, handleValidChars, handleNoMasks, handleMasks, getNewMaskCursor, testForValidChars } from './cpr-mask.helpers.js';
 
